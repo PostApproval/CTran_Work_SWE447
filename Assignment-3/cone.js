ec2342aa9c2c0014a12bc4acccdca854f24adf4c
CalvinT <43023987+PostApproval@users.noreply.github.com>
2018-09-15 11:47:34 -0700

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -3,7 +3,6 @@ var cone = null;
 
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
-    cone = new Cone (gl, n);
 
     gl = WebGLUtils.setupWebGL( canvas );
 
@@ -13,7 +12,9 @@ function init() {
     }
 
     gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
-
+    
+    cone = new Cone (gl);
+    
     render();
 }
 
