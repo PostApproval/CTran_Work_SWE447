d88acfbabaf37024bcc978660647d2e5285039fb
CalvinT <43023987+PostApproval@users.noreply.github.com>
2018-09-19 21:31:23 -0700

    Add files via upload

--- Common/Cube.js
+++ Common/Cube.js
@@ -20,40 +20,40 @@ function Cube( vertexShaderId, fragmentShaderId ) {
         values : new Float32Array([
            // Add your list vertex positions here
    // Front face
-  -1.0, -1.0,  1.0,
-   1.0, -1.0,  1.0,
-   1.0,  1.0,  1.0,
-  -1.0,  1.0,  1.0,
+   0.0,  0.0,  0.0,
+   0.5,  0.0,  0.0,
+   0.5,  0.5,  0.0,
+   0.0,  0.5,  0.0,
   
   // Back face
-  -1.0, -1.0, -1.0,
-  -1.0,  1.0, -1.0,
-   1.0,  1.0, -1.0,
-   1.0, -1.0, -1.0,
+   0.5,  0.0, 0.0,
+   0.5,  0.5, 0.0,
+   0.5,  0.5, 0.5,
+   0.5,  0.0, 0.5,
   
   // Top face
-  -1.0,  1.0, -1.0,
-  -1.0,  1.0,  1.0,
-   1.0,  1.0,  1.0,
-   1.0,  1.0, -1.0,
+   0.5,  0.0,  0.5,
+   0.0,  0.0,  0.5,
+   0.0,  0.5,  0.5,
+   0.5,  0.5,  0.5,
   
   // Bottom face
-  -1.0, -1.0, -1.0,
-   1.0, -1.0, -1.0,
-   1.0, -1.0,  1.0,
-  -1.0, -1.0,  1.0,
+   0.0,  0.0,  0.5,
+   0.0,  0.0,  0.0,
+   0.0,  0.5,  0.0,
+   0.0,  0.5,  0.5,
   
   // Right face
-   1.0, -1.0, -1.0,
-   1.0,  1.0, -1.0,
-   1.0,  1.0,  1.0,
-   1.0, -1.0,  1.0,
+   0.0,  0.5,  0.0,
+   0.5,  0.5,  0.0,
+   0.5,  0.5,  0.5,
+   0.0,  0.5,  0.5,
   
   // Left face
-  -1.0, -1.0, -1.0,
-  -1.0, -1.0,  1.0,
-  -1.0,  1.0,  1.0,
-  -1.0,  1.0, -1.0,
+   0.0,  0.0,  0.5,
+   0.5,  0.0,  0.5,
+   0.0,  0.0,  0.0,
+   0.5,  0.0,  0.0,
    ]),
         numComponents : 3
     };
@@ -61,18 +61,73 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     this.indices = { 
         values : new Uint16Array([
             // Add your list of triangle indices here
-    0,  1,  2,      0,  2,  3,    // front
-    4,  5,  6,      4,  6,  7,    // back
-    8,  9,  10,     8,  10, 11,   // top
-    12, 13, 14,     12, 14, 15,   // bottom
-    16, 17, 18,     16, 18, 19,   // right
-    20, 21, 22,     20, 22, 23,   // left
+			
+      0,  1,  3,      
+	  3,  1,  2,    // front
+	  
+      7,  5,  4,
+      6,  5,  7,	// back
+	  
+      8,  9,  11,
+	  11,  9, 10,   // top
+	  
+	  12, 13, 15,
+	  15, 13, 14,   // bottom
+	  
+      16, 17, 19,
+	  19, 17, 18,   // right
+	  
+      20, 21, 22,
+	  22, 21, 23,   // left
     ])
         
     };
-    this.indices.count = this.indices.values.length;
 
-    
+	this.color = { 
+        values : new Float32Array([
+           // Add your list vertex positions here
+   // Front face
+   0.0,  0.0,  0.0, 1.0,
+   0.5,  0.0,  0.0, 1.0,
+   0.5,  0.5,  0.0, 1.0,
+   0.0,  0.5,  0.0, 1.0,
+  
+  // Back face
+   0.5,  0.0, 0.0, 1.0,
+   0.5,  0.5, 0.0, 1.0,
+   0.5,  0.5, 0.5, 1.0,
+   0.5,  0.0, 0.5, 1.0,
+  
+  // Top face
+   0.5,  0.0,  0.5, 1.0,
+   0.0,  0.0,  0.5, 1.0,
+   0.0,  0.5,  0.5, 1.0,
+   0.5,  0.5,  0.5, 1.0,
+  
+  // Bottom face
+   0.0,  0.0,  0.5, 1.0,
+   0.0,  0.0,  0.0, 1.0,
+   0.0,  0.5,  0.0, 1.0,
+   0.0,  0.5,  0.5, 1.0,
+  
+  // Right face
+   0.0,  0.5,  0.0, 1.0,
+   0.5,  0.5,  0.0, 1.0,
+   0.5,  0.5,  0.5, 1.0,
+   0.0,  0.5,  0.5, 1.0,
+  
+  // Left face
+   0.0,  0.0,  0.5, 1.0,
+   0.5,  0.0,  0.5, 1.0,
+   0.0,  0.0,  0.0, 1.0,
+   0.5,  0.0,  0.0, 1.0,
+   ]),
+        numComponents : 4
+    };
+	
+	
+	this.indices.count = this.indices.values.length;
+  
     this.positions.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
     gl.bufferData( gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW );
