51cf720a148c7fb4043545d3d905d794c3a929ed
CalvinT <43023987+PostApproval@users.noreply.github.com>
2018-10-10 20:33:55 -0700

    Update xCube.js

--- Assignment_6/xCube.js
+++ Assignment_6/xCube.js
@@ -117,7 +117,7 @@ function Cube( vertexShaderId, fragmentShaderId ) {
 			handleLoadedTexture (texImage, texture);
 		}		
 		
-		texImage.src = "monkey.png";		
+		texImage.src = "./monkey.png";		
 	}
 	
     
@@ -188,14 +188,16 @@ function Cube( vertexShaderId, fragmentShaderId ) {
 handleLoadedTexture = function (image, texture) {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    image = texture.image;
+    //image = texture.image;
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    //if (isPowerOf2(image.width) && isPowerOf2(image.height)) 
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+		gl.generateMipmap(gl.TEXTURE_2D);
+	}
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.generateMipmap(gl.TEXTURE_2D);
+   // gl.generateMipmap(gl.TEXTURE_2D);
     gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
